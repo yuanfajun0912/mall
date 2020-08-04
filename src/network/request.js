a25fccc814f661e85d17aa25a6108c216d8bc03b
yuanfajun0912 <1654891661@qq.com>
2020-08-04 15:57:48 +0800

    update api

--- src/network/request.js
+++ src/network/request.js
@@ -5,7 +5,8 @@ export function request(config) {
   const instance1 = axios.create({
     //baseURL: 'http://123.207.32.32:8000/api/wh',
     // baseURL: 'http://106.54.54.237:8000/api/wh',
-    baseURL: 'http://123.207.32.32:8000/api/m3',
+    // baseURL: 'http://123.207.32.32:8000/api/m3',
+    baseURL: 'http://123.207.32.32:8000/api/x6',
     timeout: 8000
   })
   //请求拦截
